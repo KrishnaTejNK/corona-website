f8e23dff17f2fe5936e25d8dad3e3e7186ef4cf9
KrishnaTejNK <71250469+KrishnaTejNK@users.noreply.github.com>
2020-09-14 12:32:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import './App.css';
+import Home from './components/pages/Home'
+import Dropdown from './Dropdown';
+
+function App() {
+  return (
+    <div>
+    <Router>
+        <Navbar />
+        {/* <Dropdown /> */}
+        <Switch>
+          <Route path='/' exact component={Home} / >
+        </Switch>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
